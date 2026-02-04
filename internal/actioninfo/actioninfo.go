1cb248bd8a92d8d59d7d819f63187a49eea50a0c
Фомин Виктор Алексеевич <fominv818@gmail.com>
2026-02-04 11:52:05 +0300

    First

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,31 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	// TODO: добавить методы
+	Parse(datastring string) (err error)
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	for i := 0; i < len(dataset); i++ {
+		err := dp.Parse(dataset[i])
+		if err != nil {
+			log.Printf("Ошибка выполнения %v", err)
+			continue
+		}
+		s, errA := dp.ActionInfo()
+		if errA != nil {
+			log.Printf("Ошибка выполнения %v", err)
+			continue
+		}
+		fmt.Print(s)
+
+	}
+
 }
